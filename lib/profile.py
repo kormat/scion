e417009855ae87ab998dfa7c938783ce6dd25f30
Stephen Shirley <kormat@gmail.com>
2017-06-06 14:34:44 +0200

    pretty

--- lib/profile.py
+++ lib/profile.py
@@ -12,9 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-import tracemalloc
+# Stdlib
+import linecache
 import logging
+import os
+import tracemalloc
 
 
 def profile_start():
@@ -22,12 +24,33 @@ def profile_start():
 
 
 def profile_log_stats(n=10):
-    s = ["Memory stats"]
-    for stat in profile_stats()[:n]:
-        s.append(str(stat))
-    logging.debug("\n  ".join(s))
+    logging.debug("Memory stats. %s",
+                  filter_top(tracemalloc.take_snapshot(), limit=n))
 
 
-def profile_stats():
-    snapshot = tracemalloc.take_snapshot()
-    return snapshot.statistics('lineno')
+def filter_top(snapshot, key_type='lineno', limit=10):
+    # Adapted from https://docs.python.org/3/library/tracemalloc.html#pretty-top
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap_external>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+    s = ["Top %s lines:" % limit]
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        # replace "/path/to/module/file.py" with "module/file.py"
+        filename = os.sep.join(frame.filename.split(os.sep)[-2:])
+        s.append("#%s: %s:%s: %.1f KiB"
+                 % (index, filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            s.append('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        s.append("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    s.append("Total allocated size: %.1f KiB" % (total / 1024))
+    return "\n".join(s)
